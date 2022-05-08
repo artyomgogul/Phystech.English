fd5a414088bd08e6c29e9c278619590121a3be05
artyomgogul <104507697+artyomgogul@users.noreply.github.com>
2022-05-08 20:22:54 +0300

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,30 +1,31 @@
 import requests
 
-IAM_TOKEN = 't1.9euelZqMlo-SkcrIncubx8zNj5iRme3rnpWaj4rMk4nJy8bMmZSSlJKLkMnl8_d-SCFs-e8EegU-_t3z9z53Hmz57wR6BT7-.dnYNNW5gED11SZNTE3oe7N1ibqwIZJyf92UF5MRDHylR36cMwFXOGDvG8xstZ6mtuUS3zuBewumP5Zd800GxCg'
-folder_id = 'b1gu7o5od00898noj0su'
-target_language = 'ru'
-texts = [input()]
+def translate(texts):
+    IAM_TOKEN = 't1.9euelZqMlo-SkcrIncubx8zNj5iRme3rnpWaj4rMk4nJy8bMmZSSlJKLkMnl8_d-SCFs-e8EegU-_t3z9z53Hmz57wR6BT7-.dnYNNW5gED11SZNTE3oe7N1ibqwIZJyf92UF5MRDHylR36cMwFXOGDvG8xstZ6mtuUS3zuBewumP5Zd800GxCg'
+    folder_id = 'b1gu7o5od00898noj0su'
+    target_language = 'ru'
 
-body = {
-    "targetLanguageCode": target_language,
-    "texts": texts,
-    "folderId": folder_id,
-}
+    body = {
+        "targetLanguageCode": target_language,
+        "texts": texts,
+        "folderId": folder_id,
+    }
 
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer {0}".format(IAM_TOKEN)
-}
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {0}".format(IAM_TOKEN)
+    }
 
-response = requests.post('https://translate.api.cloud.yandex.net/translate/v2/translate',
-    json = body,
-    headers = headers
-)
+    response = requests.post('https://translate.api.cloud.yandex.net/translate/v2/translate',
+        json = body,
+        headers = headers
+    )
 
-l = response.text
+    l = response.text
 
-l = l[37:]
-l = l[:l.index("\"")]
+    l = l[37:]
+    l = l[:l.index("\"")]
+    return l
 
 print(l)
 
