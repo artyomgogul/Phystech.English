3c70bbe74e6750b7a138594c803a486a98d7e98e
anndorfman <92531551+anndorfman@users.noreply.github.com>
2022-05-11 02:14:32 +0300

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,4 +1,5 @@
 import requests
+
 def translate(texts, language = 'ru'):
     IAM_TOKEN = 't1.9euelZrKz5uNm56LyZjNj8yenMeJye3rnpWaj4rMk4nJy8bMmZSSlJKLkMnl8_c1LBZs-e8KWR1P_t3z93VaE2z57wpZHU_-.1VUlj204ehw3PdA6aESDPwX4h-5i1q1zj0r4xn-EbfTP8naOJXclgkn4L6n-oNpyMrwqO4BBmj6J1s5oCxytBw'
     folder_id = 'b1gu7o5od00898noj0su'
