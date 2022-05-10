44a9667d11956698d8c29705db68b2be6e9eb946
artyomgogul <104507697+artyomgogul@users.noreply.github.com>
2022-05-10 14:56:10 +0300

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,12 +1,11 @@
 import requests
 
-def translate(texts):
-    IAM_TOKEN = 't1.9euelZqMlo-SkcrIncubx8zNj5iRme3rnpWaj4rMk4nJy8bMmZSSlJKLkMnl8_d-SCFs-e8EegU-_t3z9z53Hmz57wR6BT7-.dnYNNW5gED11SZNTE3oe7N1ibqwIZJyf92UF5MRDHylR36cMwFXOGDvG8xstZ6mtuUS3zuBewumP5Zd800GxCg'
+def translate(texts, language):
+    IAM_TOKEN = 't1.9euelZqWmJOWx8bMjszKi5XOy5qTz-3rnpWaj4rMk4nJy8bMmZSSlJKLkMnl8_ceDRps-e9VPUEo_N3z9147F2z571U9QSj8.OCiGo0h7qv2Zk9inNAe1P7FEro19oSa4Hdl3mM1WaaV2ENktXT4jzlYCtuDpcm8NOa9AeWTPUyuXUH50CPGhDg'
     folder_id = 'b1gu7o5od00898noj0su'
-    target_language = 'ru'
 
     body = {
-        "targetLanguageCode": target_language,
+        "targetLanguageCode": language,
         "texts": texts,
         "folderId": folder_id,
     }
