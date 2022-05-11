5fbfc5a998e7a40f284caececb80ebb04ac58987
artyomgogul <104507697+artyomgogul@users.noreply.github.com>
2022-05-11 09:10:24 +0300

    Update takewords.py

--- takewords.py
+++ takewords.py
@@ -1,5 +1,5 @@
-WORDS = {'elem_en-ru':{}, 'elem_ru-en':{}, 'elem_rand':{}, 'inter_en-ru':{}, 'inter_ru-en':{}, 'inter_rand':{},
-         'upper_en-ru':{}, 'upper_ru-en':{}, 'upper_rand':{}}
+WORDS = {'eleme_en-ru':{}, 'eleme_ru-en':{}, 'eleme_rndom':{}, 'inter_en-ru':{}, 'inter_ru-en':{}, 'inter_rndom':{},
+         'upper_en-ru':{}, 'upper_ru-en':{}, 'upper_rndom':{}, 'prein_en-ru':{}, 'prein_ru-en':{}, 'prein_rndom':{}}
 
 def data_words(level):
 	with open(f'C:/Users/Human/PycharmProjects/TelegramBot/words/{level}.txt', 'r') as f:
@@ -7,9 +7,10 @@ def data_words(level):
 			WORDS[f'{level}_en-ru'].update({line[:line.index(':')].strip(): line[line.index(':') + 1:].strip()})
 			WORDS[f'{level}_ru-en'].update({line[line.index(':') + 1:].strip(): line[:line.index(':')].strip()})
 
-	WORDS[f'{level}_rand'].update(WORDS[f'{level}_en-ru'])
-	WORDS[f'{level}_rand'].update(WORDS[f'{level}_ru-en'])
+	WORDS[f'{level}_rndom'].update(WORDS[f'{level}_en-ru'])
+	WORDS[f'{level}_rndom'].update(WORDS[f'{level}_ru-en'])
 
-data_words('elem')
+data_words('eleme')
 data_words('inter')
+data_words('prein')
 data_words('upper')
